d8cb5fce2c90e1a13e1ea0bbf5bf0b8bfa370393
abdrzakk-jpg <abdrzakk.dz@gmail.com>
2026-07-23 22:17:32 +0100

    Fix: broken .env file path expression

--- app/src/dotenv_loader.py
+++ app/src/dotenv_loader.py
@@ -5,13 +5,13 @@ import os
 
 
 # load .env file
-env_path = Path(__file__).cwd() / "_internal" / ".env"
+env_path = Path(__file__).resolve().parent.parent.parent / "_internal" / ".env"
 if not os.path.exists(env_path):
-    env_path = Path(__file__).cwd() / ".env"  
-
+    env_path = Path(__file__).resolve().parent.parent.parent / ".env"  
 
 load_dotenv(str(env_path))
 
 # load EnvVars
 ip: str = os.getenv("gateaway_ip")
 check_interval_in_hours: int = int(os.getenv("check_interval_in_hours"))
+
